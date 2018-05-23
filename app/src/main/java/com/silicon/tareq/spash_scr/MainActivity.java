e1cf74d08841700b1fe9ffcb3c73deb89aea283c
Tareq3 <mti.tareq3@gmail.com>
2018-05-24 00:13:50 +0600

    Initial_Commit

---
+++ app/src/main/java/com/silicon/tareq/spash_scr/MainActivity.java
@@ -0,0 +1,32 @@
+package com.silicon.tareq.spash_scr;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout l1,l2;
+
+    Animation upToDown,downToUP;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        l1=(ConstraintLayout) findViewById(R.id.constrain_Layout1);
+        upToDown= AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        l1.setAnimation(upToDown);
+
+        l2=(ConstraintLayout) findViewById(R.id.constrain_Layout2);
+        downToUP=AnimationUtils.loadAnimation(this, R.anim.downtoup);
+        l2.setAnimation(downToUP);
+
+
+    }
+}
