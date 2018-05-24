a653d2c3ac8a5051e67e8f46ffaa4a9c41a30247
Tareq3 <mti.tareq3@gmail.com>
2018-05-25 00:15:44 +0600

    master

--- app/src/main/java/com/silicon/tareq/spash_scr/MainActivity.java
+++ app/src/main/java/com/silicon/tareq/spash_scr/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-//Added tag control to this project
+//Branch_01
 public class MainActivity extends AppCompatActivity {
 
 
