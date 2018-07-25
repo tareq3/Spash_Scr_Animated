ce7a5aec5e078d2d80ab5a55854dc28758dfeb1e
Tareq3 <mti.tareq3@gmail.com>
2018-07-25 13:33:02 +0600

    Initial branch _03 commit

--- app/src/main/java/com/silicon/tareq/spash_scr/SplashActivity.java
+++ app/src/main/java/com/silicon/tareq/spash_scr/SplashActivity.java
@@ -2,8 +2,10 @@ package com.silicon.tareq.spash_scr;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -44,16 +46,37 @@ public class SplashActivity extends AppCompatActivity {
         downToUP=AnimationUtils.loadAnimation(this, R.anim.downtoup);
         l2.setAnimation(downToUP);
 
-        //next Activity
-       final Intent intent=new Intent(this,FirstActivity.class);
 
+        //Intro Activity which will be played only single time on a device
+        final Intent intentIntro=new Intent(this,IntroActivity.class);
+
+        //First Activity
+       final Intent intentFirst=new Intent(this,FirstActivity.class);
+
+
+       //Shared pref for storing data for AppLifeTime
+       final SharedPreferences sharedPreferences =    PreferenceManager.getDefaultSharedPreferences(this);
+
+
+       //Making a some second delay call so that splash screen can be showed properly
         new Handler().postDelayed(new Runnable() { //we are using handler as thread sleep will block ui response
             @Override
             public void run() {
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    startActivity(intent,
-                            ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this).toBundle());
+
+
+                    // Check if we need to display our OnboardingFragment
+                    if (!sharedPreferences.getBoolean(
+                            "virgin", false)) {
+                        // The user hasn't seen the OnboardingFragment yet, so show it
+                        startActivity(intentIntro,
+                                ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this).toBundle());
+                    }else {
+
+                        startActivity(intentFirst,
+                                ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this).toBundle());
+                    }
                 }
             }
         }, 3500); //3.5 sec loading time fake
