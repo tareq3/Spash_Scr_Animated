e168f2632326cbf17ba3ee1029fdaf1676c860fc
Tareq3 <mti.tareq3@gmail.com>
2018-07-26 16:40:09 +0600

    Launcher_to_Main_Branch_X_v1 initial commit

---
+++ app/src/main/java/com/silicon/tareq/spash_scr/MainActivity.java
@@ -0,0 +1,16 @@
+package com.silicon.tareq.spash_scr;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
